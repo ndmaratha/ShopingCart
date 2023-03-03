2ce0ce395d1010e275347fb2c377c2fd16ddbcb7
Nayan Desai <ndmaratha01@gmail.com>
2023-03-03 23:56:57 +0530

    May be responseve design done

--- shreeganesh/src/Components/NavBar.jsx
+++ shreeganesh/src/Components/NavBar.jsx
@@ -5,6 +5,7 @@ import { ShoppingCart } from 'phosphor-react'
 function NavBar() {
   return (
     <div className='navbar'>
+   
       <div className="links">
         <Link to="/" className='s'>Shop</Link>
         <Link to="/cart" className='s' ><ShoppingCart size={32}/></Link>
